ca3d59c00875aea0ac400336e9a6b2922be5e4c3
Max Deibel <deibel.max@gmail.com>
2022-06-03 21:44:30 -0700

    Tiles form grid and color on mouse enter

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ function generateNewGrid(dimension) {
     const containerDiv = document.querySelector('div.container');
     for (let i = 0; i < dimension*dimension; i++) {
         let squareDiv = document.createElement('div');
-        squareDiv.innerText = 'hello';
+        squareDiv.classList.add('tile');
+        squareDiv.style.width = `calc(100% / ${dimension})`;
         containerDiv.appendChild(squareDiv);
     }
 }
 
 generateNewGrid(4);
+
+let tiles = document.querySelectorAll('.tile');
+tiles.forEach(tile => {
+    tile.addEventListener('mouseenter', () => {
+        tile.classList.add('touched');
+    })
+})
