6d3a0b21ceb1bb82d7c322316082e2e8d440388c
Max Deibel <deibel.max@gmail.com>
2022-06-04 21:35:59 -0700

    Add dynamic grid dimensions and styling

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 function generateNewGrid(dimension) {
+    clearExistingGrid();
+
     // Create dimension * dimension grid of square divs
     const containerDiv = document.querySelector('div.container');
-    for (let i = 0; i < dimension*dimension; i++) {
+    for (let i = 0; i < dimension * dimension; i++) {
         let squareDiv = document.createElement('div');
         squareDiv.classList.add('tile');
         squareDiv.style.width = `calc(100% / ${dimension})`;
         containerDiv.appendChild(squareDiv);
     }
+
+    registerMouseMoveListeners();
 }
 
-generateNewGrid(4);
+function clearExistingGrid() {
+    const containerDiv = document.querySelector('div.container');
+    while (containerDiv.firstChild) {
+        containerDiv.removeChild(containerDiv.firstChild);
+    }
+}
 
-let tiles = document.querySelectorAll('.tile');
-tiles.forEach(tile => {
-    tile.addEventListener('mouseenter', () => {
-        tile.classList.add('touched');
+function registerMouseMoveListeners() {
+    let tiles = document.querySelectorAll('.tile');
+    tiles.forEach(tile => {
+        tile.addEventListener('mousemove', () => {
+            tile.classList.add('touched');
+        })
     })
-})
+}
+
+generateNewGrid(50);
+registerMouseMoveListeners();
+
+let button = document.querySelector('button');
+button.addEventListener('click', () => {
+    let userInput = prompt("Enter number of squares per side: ");
+    let dimension = (userInput > 100) ? 100 : (userInput < 1) ? 1 : userInput;
+    generateNewGrid(dimension);
+});
